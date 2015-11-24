4440bd1749e78f0b6bc29781e73e2a256eb05d04
Florian Büther <buether@itm.uni-luebeck.de>
2015-11-24 13:31:49 +0100

    Formating of generated html, minor fixes in questionnaire.yml.

--- bootprint.cfg.js
+++ bootprint.cfg.js
@@ -3,6 +3,26 @@
 module.exports = {
   handlebars: {
     partials: require('path').join(__dirname, 'partials'),
-    templates: require('path').join(__dirname, 'templates')
+    templates: require('path').join(__dirname, 'templates'),
+    helpers: {
+      eachProperty: function(context, options) {
+        var ret = "";
+        for(var prop in context)
+        {
+          if (typeof context[prop] == 'object') {
+            for(var subprop in context[prop]) {
+              ret = ret + options.fn({
+                property: prop + '.' + subprop,
+                value: context[prop][subprop]
+              });
+            }
+          }
+          else {
+            ret = ret + options.fn({property:prop,value:context[prop]});
+          }
+        }
+        return ret;
+      }
+    }
   }
 };
